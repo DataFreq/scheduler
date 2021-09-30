fb1939ba19113a0ec6d551a975e0ebbe01c83046
tommy <trantommy@protonmail.ch>
2021-09-30 03:13:26 -0700

    Updated stores for form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,3 +152,7 @@ storiesOf("Appointment", module)
   .add("Status", () => <Status message="Deleting" />)
 
   .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
+
+  .add("Form", () => <Form />)
+  .add("Form-Edit", () => <Form name="Cindy" interviewers={interviewers} interviewer={3} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Form-Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
