0d72cea4d653b58dc5ff5ab339063d8ba8b50537
tommy <trantommy@protonmail.ch>
2021-10-05 21:05:42 -0700

    Added prop-types

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,49 +3,27 @@ import "components/Application.scss";
 import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import useApplicationData from "hooks/useApplicationData";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application() {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
-  const setDay = day => setState({ ...state, day })
-  const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
-
-  const schedule = appointments.map(appointment => {
-    const interview = getInterview(state, appointment.interview);
-    return (
-      <Appointment
-        key          = {appointment.id}
-        id           = {appointment.id}
-        time         = {appointment.time}
-        interview    = {interview}
-        interviewers = {interviewers}
-      />
-    )
-  })
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then(res => setState(prev => ({...prev, days: res[0].data, appointments: res[1].data, interviewers: res[2].data})));
-  }, [])
-
-  // const appList = appointments.map(app => {
-  //   return (
-  //     <Appointment
-  //     key={app.id}
-  //     {...app}
-  //     />
-  //   )
-  // })
+  const appointments = getAppointmentsForDay(state, state.day).map(
+    appointment => {
+      return (
+        <Appointment
+          key={appointment.id}
+          {...appointment}
+          interview={getInterview(state, appointment.interview)}
+          interviewers={interviewers}
+          bookInterview={bookInterview}
+          cancelInterview={cancelInterview}
+        />
+      );
+    }
+  );
 
   return (
     <main className="layout">
@@ -70,7 +48,7 @@ export default function Application() {
         />
       </section>
       <section className="schedule">
-        {schedule}
+        {appointments}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,38 +1,57 @@
 import React from "react";
 import "./styles.scss"
+import Confirm from "./Confirm"
+import Empty from "./Empty";
+import Error from "./Error";
+import Form from "./Form";
 import Header from "./Header";
 import Show from "./Show";
-import Empty from "./Empty";
-import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
-
 export default function Appointment(props) {
+  const CONFIRM = 'CONFIRM'
   const EMPTY = 'EMPTY';
+  const ERROR = 'ERROR';
   const SHOW = 'SHOW';
   const CREATE = 'CREATE';
-
+  const SAVING = 'SAVING';
+  const EDIT = 'EDIT';
+  
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
-  );  
+  );
+  const onAdd = () => transition(CREATE);
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name,
+      interviewer
+    };
 
+    transition(SAVING)
+    
+    props.bookInterview(props.id, interview)
+      .then(transition(SHOW))
+  };
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === EMPTY && <Empty onAdd = {onAdd} />}
       {mode === SHOW && (
         <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          student = {props.interview.student}
+          interviewer = {props.interview.interviewer}
         />
       )}
       {mode === CREATE && (
         <Form
-          interview = {props.interview}
           interviewers = {props.interviewers}
-          onCancel = {() => back()}
+          onCancel = {back}
+          onSave = {save}
         />
       )}
+      {mode === SAVING && <Status message="Saving"/>}
     </article>
   );
 };
